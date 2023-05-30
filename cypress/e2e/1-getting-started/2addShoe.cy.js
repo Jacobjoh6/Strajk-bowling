a0b6c4a426fae1276d45ca04158233debfd6cf7e
Jacobjoh6 <114565026+Jacobjoh6@users.noreply.github.com>
2023-05-30 16:53:54 +0200

    2o3 typ klara

--- cypress/e2e/1-getting-started/2addShoe.cy.js
+++ cypress/e2e/1-getting-started/2addShoe.cy.js
@@ -4,7 +4,27 @@ describe('example to-do app', () => {
       cy.visit('http://localhost:5173/')
     })
 
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
+    it('inputs initial booking data', () => {
+      cy.get('[name="when"]').type('2023-05-30').should('have.value', '2023-05-30');
+      cy.get('[name="time"]').type('18:00').should('have.value', '18:00');
+      cy.get('[name="people"]').type('3').should('have.value', 3)
+      cy.get('[name="lanes"]').type('1').should('have.value', 1 )
     })
+
+    it('Adds the same ammount of shoes as players', () => {
+      cy.get('[name="when"]').type('2023-05-30').should('have.value', '2023-05-30');
+      cy.get('[name="time"]').type('18:00').should('have.value', '18:00');
+      cy.get('[name="people"]').type('2').should('have.value', 2)
+      cy.get('[name="lanes"]').type('1').should('have.value', 1 )
+
+      cy.get('.shoes__button').click().click()
+
+      cy.get('.shoes__form').first().type('43')
+      cy.get('.shoes__form').last().type('45')
+    })
+
+
 })
+    it('', () => {
+      
+    })
--- cypress/e2e/1-getting-started/3removeShoe.cy.js
+++ cypress/e2e/1-getting-started/3removeShoe.cy.js
@@ -3,4 +3,17 @@ describe('example to-do app', () => {
 
       cy.visit('http://localhost:5173/')
     })
+
+    it('Adds the same ammount of shoes as players', () => {
+      cy.get('[name="when"]').type('2023-05-30').should('have.value', '2023-05-30');
+      cy.get('[name="time"]').type('18:00').should('have.value', '18:00');
+      cy.get('[name="people"]').type('2').should('have.value', 2)
+      cy.get('[name="lanes"]').type('1').should('have.value', 1 )
+
+      cy.get('.shoes__button').click().click().click()
+
+      cy.get('.shoes__form').first().type('43')
+      cy.get('.shoes__form').eq(-2).find('button').click()
+      cy.get('.shoes__form').last().type('45')
+    })
 })
