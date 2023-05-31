bb549c0135d7cf04edaa9fe466a9b533c0bbff59
Jacobjoh6 <114565026+Jacobjoh6@users.noreply.github.com>
2023-05-31 12:21:18 +0200

    nästan klar

--- cypress/e2e/1-getting-started/3removeShoe.cy.js
+++ cypress/e2e/1-getting-started/3removeShoe.cy.js
@@ -4,7 +4,23 @@ describe('example to-do app', () => {
       cy.visit('http://localhost:5173/')
     })
 
-    it('Adds the same ammount of shoes as players', () => {
+    it('Adds a number of players', () => {
+      cy.get('[name="when"]').type('2023-05-30').should('have.value', '2023-05-30');
+      cy.get('[name="time"]').type('18:00').should('have.value', '18:00');
+      cy.get('[name="people"]').type('2').should('have.value', 2)
+      cy.get('[name="lanes"]').type('1').should('have.value', 1 )
+    })
+
+    it('Adds one more pair of shoes than players', () => {
+      cy.get('[name="when"]').type('2023-05-30').should('have.value', '2023-05-30');
+      cy.get('[name="time"]').type('18:00').should('have.value', '18:00');
+      cy.get('[name="people"]').type('2').should('have.value', 2)
+      cy.get('[name="lanes"]').type('1').should('have.value', 1 )
+
+      cy.get('.shoes__button').click().click().click()
+    })
+
+    it('Removes the extra pair', () => {
       cy.get('[name="when"]').type('2023-05-30').should('have.value', '2023-05-30');
       cy.get('[name="time"]').type('18:00').should('have.value', '18:00');
       cy.get('[name="people"]').type('2').should('have.value', 2)
--- cypress/e2e/1-getting-started/4reserve.cy.js
+++ cypress/e2e/1-getting-started/4reserve.cy.js
@@ -3,4 +3,34 @@ describe('example to-do app', () => {
 
       cy.visit('http://localhost:5173/')
     })
+
+    it('Reserves booking', () => {
+      cy.get('[name="when"]').type('2023-05-30').should('have.value', '2023-05-30');
+      cy.get('[name="time"]').type('18:00').should('have.value', '18:00');
+      cy.get('[name="people"]').type('2').should('have.value', 2)
+      cy.get('[name="lanes"]').type('1').should('have.value', 1 )
+
+      cy.get('.shoes__button').click().click()
+
+      cy.get('.shoes__form').first().type('43')
+      cy.get('.shoes__form').last().type('45')
+
+      cy.get('.booking').find('button').last().click()
+    })
+
+    it('Confirms booking and recieves confirmation', () => {
+      cy.get('[name="when"]').type('2023-05-30').should('have.value', '2023-05-30');
+      cy.get('[name="time"]').type('18:00').should('have.value', '18:00');
+      cy.get('[name="people"]').type('2').should('have.value', 2)
+      cy.get('[name="lanes"]').type('1').should('have.value', 1 )
+
+      cy.get('.shoes__button').click().click()
+
+      cy.get('.shoes__form').first().type('43')
+      cy.get('.shoes__form').last().type('45')
+
+      cy.get('.booking').find('button').last().click()
+
+      cy.get('.confirmation__details').find('button').click()
+    }) 
 })
